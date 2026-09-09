f64b0514987bcd67519cfbb6600a3c14e5552817
Anna Li <anna.li.4725@gmail.com>
2026-09-10 02:33:10 +0500

    fix: update Playwright tests and HAR mocks

--- tests/constructor.pl.tsx
+++ tests/constructor.pl.tsx
@@ -1,13 +1,21 @@
 import { expect, test } from '@playwright/test';
 
-test.describe('Конструктор бургера', () => {
-  test('добавляет начинку из списка в конструктор', async({ page }) => {
-    await page.routeFromHAR('./tests/hars/burger-api.har', {
-      url: '**/api/**',
-      update: false
-    });
+test.beforeEach(async ({ page }) => {
+  await page.routeFromHAR('./tests/hars/burger-api.har', {
+    url: '**/api/**',
+    update: false
+  });
+});
 
+const orderNumber = 110021;
+
+test.describe('Конструктор бургера', () => {
+  test.beforeEach(async({ page }) => {
     await page.goto('/');
+  });
+
+  test('добавляет начинку из списка в конструктор', async({ page }) => {
+    const constructor = page.getByTestId('burger-constructor');
 
     const ingredient = page
       .locator('li')
@@ -15,22 +23,26 @@ test.describe('Конструктор бургера', () => {
         hasText: 'Биокотлета из марсианской Магнолии'
       });
 
-    await ingredient.getByRole('button', { name: 'Добавить' }).click();
-
-    await expect(page.getByText(
+    await expect(constructor.getByText(
       'Биокотлета из марсианской Магнолии',
       {exact: true})
-    ).toHaveCount(2);
+    ).toHaveCount(0);
+
+    await ingredient.getByRole('button', { name: 'Добавить' }).click();
+
+    await expect(constructor.getByText(
+      'Биокотлета из марсианской Магнолии',
+      {exact: true})
+    ).toHaveCount(1);
   });
 });
 
 test.describe('Модальное окно ингредиента', () => {
-  test('открывает модальное окно с ингредиентом', async ({ page }) => {
-    await page.routeFromHAR('./tests/hars/burger-api.har', {
-      url: '**/api/**',
-      update: false
-    });
+  test.beforeEach(async ({ page }) => {
     await page.goto('/');
+  });
+
+  test('открывает модальное окно с ингредиентом', async ({ page }) => {
     const ingredient = page
       .locator('li')
       .filter({
@@ -38,17 +50,15 @@ test.describe('Модальное окно ингредиента', () => {
       });
 
     await ingredient.getByRole('link').click();
-    await expect(page.getByText('Детали ингредиента', {exact: true})).toBeVisible();
 
-    await expect(page.getByRole('heading', {name: 'Биокотлета из марсианской Магнолии'})).toBeVisible();
+    const modal = page.getByTestId('modal');
+
+    await expect(modal.getByText('Детали ингредиента', {exact: true})).toBeVisible();
+
+    await expect(modal.getByRole('heading', {name: 'Биокотлета из марсианской Магнолии'})).toBeVisible();
   });
 
   test('закрывает модальное окно по крестику', async ({ page }) => {
-    await page.routeFromHAR('./tests/hars/burger-api.har', {
-      url: '**/api/**',
-      update: false
-    });
-    await page.goto('/');
     const ingredient = page
       .locator('li')
       .filter({
@@ -56,18 +66,17 @@ test.describe('Модальное окно ингредиента', () => {
       });
 
     await ingredient.getByRole('link').click();
-    await expect(page.getByText('Детали ингредиента', {exact: true})).toBeVisible();
 
-    await page.getByRole('button', { name: 'Закрыть модальное окно' }).click();
-    await expect(page.getByRole('heading', {name: 'Биокотлета из марсианской Магнолии'})).not.toBeVisible();
+    const modal = page.getByTestId('modal');
+
+    await expect(modal).toBeVisible();
+
+    await modal.getByRole('button', { name: 'Закрыть модальное окно' }).click();
+
+    await expect(modal).not.toBeVisible();
   });
 
   test('закрывает модальное окно по оверлею', async ({ page }) => {
-    await page.routeFromHAR('./tests/hars/burger-api.har', {
-      url: '**/api/**',
-      update: false
-    });
-    await page.goto('/');
     const ingredient = page
       .locator('li')
       .filter({
@@ -75,62 +84,20 @@ test.describe('Модальное окно ингредиента', () => {
       });
 
     await ingredient.getByRole('link').click();
-    await expect(page.getByText('Детали ингредиента', {exact: true})).toBeVisible();
+
+    const modal = page.getByTestId('modal');
+
+    await expect(modal).toBeVisible();
 
     await page.getByTestId('modal-overlay').click({
       position: { x: 10, y: 10 }
     });
-    await expect(page.getByText('Детали ингредиента', { exact: true })).not.toBeVisible();
+    await expect(modal).not.toBeVisible();
   });
 });
 
 test.describe('Оформление заказа', () => {
-  test('оформляет заказ и очищает конструктор', async({context, page}) => {
-    await page.routeFromHAR('./tests/hars/burger-api.har', {
-      url: '**/api/**',
-      update: false
-    });
-
-    await page.route('**/api/auth/user', async (route) => {
-      await route.fulfill({
-        status: 200,
-        contentType: 'application/json',
-        body: JSON.stringify({
-          success: true,
-          user: {
-            email: 'test@mail.com',
-            name: 'Person1'
-          }
-        })
-      });
-    });
-
-    await page.route('**/api/orders', async (route) => {
-      await route.fulfill({
-        status: 200,
-        contentType: 'application/json',
-        body: JSON.stringify({
-          success: true,
-          order: {
-            _id: 'test-order-id',
-            status: 'done',
-            name: 'Space Burger',
-            owner: {
-              name: 'Person1',
-              email: 'test@mail.com',
-              createdAt: '2026-09-08',
-              updatedAt: '2026-09-08'
-            },
-            createdAt: '2026-09-08',
-            updatedAt: '2026-09-08',
-            number: 12345,
-            price: 25746
-          },
-          name: 'Space Burger'
-        })
-      });
-    });
-
+  test.beforeEach(async ({ context, page }) => {
     await context.addCookies([
       {
         name: 'accessToken',
@@ -141,11 +108,13 @@ test.describe('Оформление заказа', () => {
     ]);
 
     await page.addInitScript(() => {
-      localStorage.setItem('refreshToken', 'fake-refresh-token')
+      localStorage.setItem('refreshToken', 'fake-refresh-token');
     });
 
     await page.goto('/');
+  });
 
+  test('оформляет заказ и очищает конструктор', async({ page }) => {
     const bun = page
       .locator('li')
       .filter({
@@ -164,10 +133,15 @@ test.describe('Оформление заказа', () => {
 
     await page.getByRole('button', { name: 'Оформить заказ' }).click();
 
-    await expect(page.getByRole('heading', {name: '12345'})).toBeVisible();
-    await expect(page.getByText('Выберите булки', {exact: true})).toHaveCount(2);
-    await expect(page.getByText('Выберите начинку', {exact: true})).toBeVisible();
-    await page.getByRole('button', {name: 'Закрыть модальное окно'}).click();
-    await expect(page.getByRole('heading', { name: '12345' })).not.toBeVisible();
+    const orderModal = page.getByTestId('modal');
+
+    await expect(orderModal.getByRole('heading', {name: String(orderNumber)})).toBeVisible();
+
+    const constructor = page.getByTestId('burger-constructor');
+
+    await expect(constructor.getByText('Выберите булки', {exact: true})).toHaveCount(2);
+    await expect(constructor.getByText('Выберите начинку', {exact: true})).toBeVisible();
+    await orderModal.getByRole('button', {name: 'Закрыть модальное окно'}).click();
+    await expect(orderModal).not.toBeVisible();
   });
 })
